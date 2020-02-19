5da01afae9dbde1adc61e3015721a9512648a030
Nandan <nandanmarkrao@gmail.com>
2020-02-19 01:15:52 +0100

    added kafka as operator for agrius

--- setup-kube.sh
+++ setup-kube.sh
@@ -1,49 +1,61 @@
-
 # Manually:
 # Create cluster in UI
 # Run:
 # gcloud --project $PROJECT compute addresses create $CLUSTER_NAME-ip --region europe-west1
 
-# PROJECT=toixotoixo
-# CLUSTER_NAME=toixo-cluster
+PROJECT=trollhunters
+CLUSTER_NAME=hunting
+ZONE=europe-west1-b
 # STATIC_IP=34.76.183.123
 
-# # gcloud container clusters create my-regional-cluster --num-nodes 2 --region europe-west1 \
-# # --node-locations europe-west1-b,europe-west1-c
+gcloud --project $PROJECT container clusters create $CLUSTER_NAME --num-nodes 3 --machine-type n1-highmem-2 --zone $ZONE
 
-# gcloud --project $PROJECT container clusters get-credentials --region europe-west1 $CLUSTER_NAME
+gcloud --project $PROJECT container clusters get-credentials --zone $ZONE $CLUSTER_NAME
 
 
 # secrets
-# kubectl create secret generic agrius-logstash-keys --from-file=../dash/logstash/keys
-# kubectl create secret generic agrius-jaws-keys --from-file=../jaws/keys
-# kubectl create secret generic agrius-belly-keys --from-file=../bellypy/keys
-# kubectl create secret generic agrius-bellyback-keys --from-file=../belly/keys
-# kubectl create secret generic agrius-claws-keys --from-file ../claws/keys
-# kubectl create secret generic agrius-jaws-envs --from-env-file ../jaws/.env
-# kubectl create secret generic agrius-claws-envs --from-env-file ../claws/.env
+kubectl create secret generic agrius-logstash-keys --from-file=../dash/logstash/keys
+kubectl create secret generic agrius-jaws-keys --from-file=../jaws/keys
+kubectl create secret generic agrius-belly-keys --from-file=../bellypy/keys
+kubectl create secret generic agrius-bellyback-keys --from-file=../belly/keys
+kubectl create secret generic agrius-claws-keys --from-file ../claws/keys
+kubectl create secret generic agrius-jaws-envs --from-env-file ../jaws/.env
+kubectl create secret generic agrius-claws-envs --from-env-file ../claws/.env
+kubectl create secret generic agrius-dig-keys --from-file=../dig/digger/keys
 
 
 # SSD
-# kubectl create -f ssd-storage.yaml
+kubectl create -f ssd-storage.yaml
 
+# Namespaces
+kubectl create namespace cert-manager
+kubectl create namespace monitoring
 
 # # Install nginx-ingress
-# helm install nginx-ingress stable/nginx-ingress --set rbac.create=true
+helm install nginx-ingress stable/nginx-ingress --set rbac.create=true
 
 ## Install cert-manager
-# kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.13.0/deploy/manifests/00-crds.yaml
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.13.0/deploy/manifests/00-crds.yaml
 
-# kubectl create namespace cert-manager
-# helm repo add jetstack https://charts.jetstack.io
-# helm repo update
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
 
-# helm install cert-manager --namespace cert-manager \
-  # --version v0.13.0 \
-  # jetstack/cert-manager
+helm install cert-manager --namespace cert-manager \
+  --version v0.13.0 \
+  jetstack/cert-manager
 
 # wait for it to be ready
-# sleep 15
+sleep 30
 
-# # Create cluster-issuer
-# kubectl create -f cm-issuer.yaml
+# Create cluster-issuer
+kubectl create -f cm-issuer.yaml
+
+# Create Prometheus Operator
+helm install --namespace monitoring prometheus stable/prometheus-operator -f values/prometheus.yaml
+
+# Create zookeeper operator and kafka operator
+helm install zookeeper-operator banzaicloud-stable/zookeeper-operator
+helm install kafka-operator banzaicloud-stable/kafka-operator
+
+
+
